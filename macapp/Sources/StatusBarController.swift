cfc0f9b1cee160a25012ea1f506cd4613c9fbb0f
MrGameVlogger <46544811+MrGameVlogger@users.noreply.github.com>
2026-08-17 00:52:51 -0400

    fix: revert status bar icon back to SF Symbol
    NSApp.applicationIconImage doesn't work reliably for status bar icons.
    Reverting to the original SF Symbol approach.

--- macapp/Sources/StatusBarController.swift
+++ macapp/Sources/StatusBarController.swift
@@ -28,10 +28,7 @@ class StatusBarController: NSObject, NSMenuDelegate {
         statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
 
         if let button = statusItem.button {
-            if let appIcon = NSApp.applicationIconImage {
-                appIcon.isTemplate = true
-                button.image = appIcon
-            } else if let image = NSImage(systemSymbolName: "play.circle", accessibilityDescription: "Jellyfin MPV Play") {
+            if let image = NSImage(systemSymbolName: "play.circle", accessibilityDescription: "Jellyfin MPV Play") {
                 image.isTemplate = true
                 button.image = image
             }
